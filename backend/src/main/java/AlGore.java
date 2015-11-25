6811bd7ece52e82f5461f82f6648891a722adb71
Eugene Kolo <ekolodenker@mitre.org>
2015-11-24 21:32:48 -0500

    Changed routes to prefix with /api/. Server now runs on localhost:5000

--- backend/src/main/java/AlGore.java
+++ backend/src/main/java/AlGore.java
@@ -1,7 +1,28 @@
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 import static spark.Spark.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class AlGore {
+
+	public static MustacheTemplateEngine mustache = new MustacheTemplateEngine("../../../frontend");
+
     public static void main(String[] args) {
-        get("/", (req, res) -> "Hello, World!");
+    	/* Spark Configuration */
+    	setPort(5000); // Run on port 5000
+    	setIpAddress("127.0.0.1"); // Run on localhost
+        externalStaticFileLocation("../frontend");
+
+        System.out.println(AlGore.class.getResource(AlGore.class.getSimpleName() + ".class"));
+
+        post("/api/addPlaylist", (req, res) -> {
+             return "Hello World";
+        });
+
+
     }
-}
+
+} // END Class AlGore
