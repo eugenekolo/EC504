fc97a9ccb8215a136a13f5a1de3a5a4ce245d2ff
braxtonb <braxtonbrewton@gmail.com>
2015-11-29 07:07:05 -0500

    Autocomplete works, must enter songs with Title#Author. Suggested playlist now puts each title/author on separate lines.

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -41,14 +41,20 @@ $(function() {
         suggestPlaylist
     *********************/
     function suggestPlaylist() {
-        if ($('#enter-song').val() === '') {
+        var parseTitle = $('#enter-song').val().replace(/\\/g, '\\/').split('#');
+        var title = parseTitle[0];
+        if (title === '') {
             console.log("LET ME KNOW");
-            $('#suggested-playlist').text('');
-            $('.suggested-popularity').text('');
+            $('.author-suggest').text('');
+            removeTableRows();
+            var author = $('#enter-song').val().split('#').pop();
+            if (!(author === '')) {
+                $('.song-suggest').text('No playlist found');
+            } else {
+                $('.song-suggest').text('');
+            }
         } else {
             var data = {};
-            var parseTitle = $('#enter-song').val().replace(/\\/g, '\\/').split('#');
-            var title = parseTitle[0];
             var author = $('#enter-song').val().split('#').pop();
             // Gets the song name text
             data['song'] = {
@@ -67,19 +73,22 @@ $(function() {
                     // On success, below jquery suggests playlist with highest popularity containing entered song
                     console.log('Success!');
                     var hash = JSON.parse(data);
-                    // console.log(hash['songList'][0]);
-                    $('#suggested-playlist').text('');
+                    // console.log(hash['songList'][0]); 
+                    removeTableRows();
                     for (var i in hash['songList']) {
-                        var currentPlaylistText = $('#suggested-playlist').text();
-                        currentPlaylistText += hash['songList'][i]['title'] + ' by ' + hash['songList'][i]['author'] + ', ';
-                        $('#suggested-playlist').text(formatPlaylist(currentPlaylistText));
+                        $('.song-suggest:last').text(formatPlaylist(hash['songList'][i]['title']));
+                        $('.author-suggest:last').text(formatPlaylist(hash['songList'][i]['author']));
+                        addTableRows();
+                        addTableRowsClasses();
                     }
-                    $('#suggested-playlist').text($('#suggested-playlist').text().slice(0, -2));
+                    // console.log($('.song-suggest:first').text());
+                    // $('.playlist-row:first').remove();
                 },
                 error: function(data) {
                     // alert('Failed to retrieve Top8 content');
                     console.log('Failed to retrieve suggestPlaylist content');
-                    $('#suggested-playlist').text('No Playlist Found');
+                    removeTableRows();
+                    $('.song-suggest').text('No Playlist Found');
                 }
             });
         }
@@ -88,14 +97,14 @@ $(function() {
         getAutocomplete
     *********************/
     function autocomplete() {
-        if ($('#enter-song').val() === '') {
+        var parseTitle = $('#enter-song').val().replace(/\\/g, '\\/').split('#');
+        var title = parseTitle[0];
+        if (title === '') {
             console.log("LET ME KNOW");
             $('.song-autocomplete').text('');
             $('.author-autocomplete').text('');
         } else {
             var data = {};
-            var parseTitle = $('#enter-song').val().replace(/\\/g, '\\/').split('#');
-            var title = parseTitle[0];
             data['song'] = title; // Gets the song name text
             // console.log(data['song']);
             $.ajax({
@@ -140,6 +149,27 @@ $(function() {
         playlist = playlist.replace(/\\/g, '');
         return playlist;
     }
+
+    function addTableRows() {
+        var songRow = songListTable.insertRow(); // Add to bottom
+        var songTitleCell = songRow.insertCell(0);
+        var songAuthorCell = songRow.insertCell(1);
+    }
+
+    function addTableRowsClasses() {
+        // Assigns class names to dynamically generated rows
+        $('#songListTable tbody tr td').attr('class', ''); // remove all previous classes
+        $('#songListTable tbody tr').attr('class', 'playlist-row');
+        $('#songListTable tbody tr td:even').attr('class', 'song-suggest');
+        $('#songListTable tbody tr td:odd').attr('class', 'author-suggest');
+        $('#songListTable thead tr').attr('class', 'playlist-header'); //
+    }
+
+    function removeTableRows() {
+        $('.playlist-row:first').attr('class', 'temp');
+        $('.playlist-row').remove();
+        $('.playlist-row:first').attr('class', 'playlist-row');
+    }
     /*************************************
      * Event Listeners
      **************************************/
@@ -170,7 +200,7 @@ $(function() {
     // });
     // Clicking on an autocomplete suggestion changes the enter song text
     // to the autocomplete suggestion and updates the suggested playlist
-    $('.playlist-row').click(function() {
+    $('.autocomplete-row').click(function() {
         var enterSong = $('#enter-song');
         var song = $(this).children('.song-autocomplete').text();
         var author = $(this).children('.author-autocomplete').text();
